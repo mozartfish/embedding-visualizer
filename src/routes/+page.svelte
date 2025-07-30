ac8be888aa81da2849868e1b6cae52cd6e62b28d
Pranav Rajan <rajan.pranav320@gmail.com>
2025-07-30 16:49:41 +0200

    fix data problem

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -627,5 +627,5 @@
     font-size: 12px;
   }
 </style>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
