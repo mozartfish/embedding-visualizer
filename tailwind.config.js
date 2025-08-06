c6f37b4fe455bc6d9bf737cc282d1a70e05269c8
Pranav Rajan <rajan.pranav320@gmail.com>
2025-08-06 21:41:29 +0200

    fix tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite/**/*.js'],
-	theme: {
-		extend: {}
-	},
-	plugins: [require('flowbite/plugin')]
+	plugins: {
+		'@tailwindcss/postcss': {},
+		autoprefixer: {}
+	}
 };
